7e04788c918e927800022395e9f12a947c0432e1
mado-bohsali <mado.bohsali@gmail.com>
2019-07-05 10:56:12 +0300

    Add files via upload

---
+++ Classes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+class Config{
+    //SMTP Mail server settings
+    const SMTP_HOST = 'smtp.mailtrap.io';
+    const SMTP_PORT = 25; //25 or 465 or 587 or 2525
+    const SMTP_USERNAME = 'c2a0f49195033a';
+    const SMTP_PASSWORD = '3285158956df75';
+}
+
+?>
